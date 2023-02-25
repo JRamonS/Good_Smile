a865b90efd97ba3ca8e0b962aba2884e98dcf65f
Ramon <ramon.r.santana@gmail.com>
2023-02-25 20:13:13 +0100

    feat: add index.js inside models and creation root for get and post

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const db = require('./db/db');
-
-
+const {Rol} = require ("./models/index");
 const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
 
 app.get("/users", (req,res) =>{
     return res.send("Vemos los usuarios")
 })
 
-app.use(express.json());
+app.post("/rols", async (req,res) =>{
 
-const PORT = 3000;
+    const { privilege } = req.body;
+
+    const newRol = {
+        privilege: privilege
+    }
+
+    // Guardar la informacion
+    const rol = await Rol.create(newRol)
+
+    return res.json(rol)
+
+})
+
+
+
+
+
+
+
+
+
 
 //Starting server
 app.listen(PORT, () => console.log("Server on port " + PORT));
