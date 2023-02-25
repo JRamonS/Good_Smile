42fbab06e3283577cd5f8693029f32bcda0f0b1f
Ramon <ramon.r.santana@gmail.com>
2023-02-25 18:37:53 +0100

    feat:add pacients and historty model and respective migration

---
+++ models/pacient.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pacient extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pacient.init({
+    name: DataTypes.STRING,
+    surname: DataTypes.STRING,
+    email: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    date_of_birth: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    postcode: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pacient',
+  });
+  return Pacient;
+};
