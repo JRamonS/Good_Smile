6faefa4c12a19cbb98ecd9175c75af7b3bf329c5
Ramon <ramon.r.santana@gmail.com>
2023-02-25 18:57:29 +0100

    feat: add appointment model and respective migration and foringkey

---
+++ migrations/20230225174522-create-appointment.js
@@ -0,0 +1,58 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Appointments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      pacient_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Pacients",
+          key: "id"
+        }
+      },
+      dentist_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Dentists",
+          key: "id"
+        }
+      },
+      treatment_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Treatments",
+          key: "id"
+        }
+      },
+      hour: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      observations: {
+        type: Sequelize.TEXT
+      },
+      date: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Appointments');
+  }
+};
