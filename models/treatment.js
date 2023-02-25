5c89b717f9bb9c94cbcbd306564b279260a40725
adrian salona <Adrian>
2023-02-25 17:58:30 +0100

    The treatment model is added with its migration

---
+++ models/treatment.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Treatment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Treatment.init({
+    name: DataTypes.STRING,
+    duration: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    description: DataTypes.TEXT,
+    date: DataTypes.STRING,
+    session_num: DataTypes.INTEGER,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Treatment',
+  });
+  return Treatment;
+};
